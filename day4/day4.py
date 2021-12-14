e7d6df7f3a81e988ef44c9cfebe3c3edf1d4f2d1
Sebastian Nilsson <seblerr@gmail.com>
2021-12-14 13:39:58 +0100

    finish day 4

--- day4/day4.py
+++ day4/day4.py
@@ -1,13 +1,35 @@
-# import re
-
 file = open("day4/input.txt", "r")
-# file = "day4/input.txt"
+
+def check_row(board):
+    for row in board:
+        ok = True
+        for r in row:
+            if not r[1]:
+                ok = False
+        if ok:
+            return True
+
+def check_column(board):
+    for i in range(5):
+        ok = True
+        for c in board:
+            if not c[i][1]:
+                ok = False
+        if ok:
+            return True
+
+def sum_unmarked(n, board):
+    sum = 0
+    for b in board:
+        for val in b:
+            if not val[1]:
+                sum += int(val[0])
+    return sum * n
 
 lines = file.readlines()
 numbers = lines[0].split(",")
 board = []
 boards = []
-
 
 for line in lines[1:]:
     line = line.strip()
@@ -19,51 +41,16 @@ for line in lines[1:]:
         board = []
 boards.append(board)
 
-def check_row(row):
-    # print(row)
-    for r in row:
-        if r[1] == False:
-            return False
-    return True
-
-def check_column(board):
-    for i in range(5):
-        for col in board:
-            print(col[i][0])
-            if col[i][1] == False:
-                # print("false", col)
-                return False
-    print("TRUE")
-    return True
-    # for i, c in enumerate(board):
-    #     for x in range(5):
-    #         print(i,x)
-    #         if not c[x][i][1]:
-    #             print("False", c)
-    #             return False
-    #         if li[c][1] == False:
-    #             return False
-    # print("found winning column")
-    # return True
-
-
-# print(numbers)
-
-def main():
-    for n in numbers:
-        for b in boards:
-            for row in b:
-                for x in row:
-                    if n==x[0]:
-                        x[1] = True
-                        if check_row(row):
-                            return n, b
-                        if check_column(b):
-                            return n, b
-
-win_n, win_b = main()
-# print(win_n, win_b)
-
-
-# for b in boards:
-#     print(b)
+boardWins = [False for _ in range(len(boards))]
+for n in numbers:
+    for i,b in enumerate(boards):
+        for row in b:
+            for x in row:
+                if n == x[0]:
+                    x[1] = True
+        if check_row(b) or check_column(b):
+            if not boardWins[i]:
+                boardWins[i] = True
+                numWin = len([j for j in range(len(boards)) if boardWins[j]])
+                if numWin == 1 or numWin == len(boards):
+                    print(sum_unmarked(int(n), b))
